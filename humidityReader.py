295b2c6302de7fe9b94cb7bc527636086dd687e9
Gustavo Mitt <gustavo.mitt@gmail.com>
2016-05-15 21:11:02 -0300

    Add rest code

---
+++ humidityReader.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import requests
+
+arduinoName = "WIZnet0EFE40"
+
+response = requests.get(arduinoName)
+
+assert response.status_code == 200
+
+for repo in response.json():
+    print '[{}] {}'.format(repo['language'], repo['name'])
