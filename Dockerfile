b08115c214887efcfe1439f129a71815a1047850
Gustavo Mitt <gustavo.mitt@gmail.com>
2016-05-17 19:46:54 -0300

    Add send humidity to message queue

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM ubuntu:xenial
 
+# Sets the Application Default Credentials
+
+ENV GOOGLE_APPLICATION_CREDENTIALS /home/GardenControlArduino-75b43a070e42.json
+
 # Update base system repos
 RUN apt-get -y update
 
@@ -9,7 +13,8 @@ RUN apt-get install -y git \
        && apt-get install -y python-pip
 
 # Install python modules
-RUN pip install requests
+RUN pip install requests \
+       && pip install --upgrade google-api-python-client
 
 # Get the program
 RUN git clone https://github.com/gustavomitt/humidityReader.git
--- humidityReader.py
+++ humidityReader.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 import requests
+import httplib2
+import base64
+
+from apiclient import discovery
+from oauth2client import client as oauth2client
+
+PUBSUB_SCOPES = ['https://www.googleapis.com/auth/pubsub']
+
+def create_pubsub_client(http=None):
+    credentials = oauth2client.GoogleCredentials.get_application_default()
+    if credentials.create_scoped_required():
+        credentials = credentials.create_scoped(PUBSUB_SCOPES)
+    if not http:
+        http = httplib2.Http()
+    credentials.authorize(http)
+
+    return discovery.build('pubsub', 'v1', http=http)
 
 arduinoName = "WIZnet0EFE40"
 
@@ -10,3 +27,20 @@ assert response.status_code == 200
 resp = response.json()
 
 humidity = resp['variables']['humidity']
+
+# Send the humidity value to message queue
+client = create_pubsub_client()
+
+message1 = base64.b64encode(humidity)
+
+body = {
+    'messages': [
+        {'data': message1},
+    ]
+}
+
+resp = client.projects().topics().publish(
+    topic='projects/gardencontrolarduino/topics/humidity', body=body).execute()
+
+
+
