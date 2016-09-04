3fe1776ee798ac284d6135c2529b2af566d46279
Gustavo Mitt <gustavo.mitt@gmail.com>
2016-09-04 08:08:13 -0300

    Create Credentials

---
+++ createCredentials.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /home
+
+> GardenControlArduino-75b43a070e42.json
+
+echo '{' >> GardenControlArduino-75b43a070e42.json
+echo '  "type": "'$type'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "project_id": "'$project_id'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "private_key_id": "'$private_key_id'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "private_key": "'$private_key'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "client_email": "'$client_email'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "client_id": "'$client_id'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "auth_uri": "'$auth_uri'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "token_uri": "'$token_uri'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "auth_provider_x509_cert_url": "'$auth_provider_x509_cert_url'",' >> GardenControlArduino-75b43a070e42.json
+echo '  "client_x509_cert_url": "'$client_x509_cert_url'"' >> GardenControlArduino-75b43a070e42.json
+echo '}' >> GardenControlArduino-75b43a070e42.json
--- humidityReader/humidityReader.py
+++ humidityReader/humidityReader.py
@@ -4,19 +4,26 @@ import httplib2
 import base64
 import datetime
 import os
+import cronus.beat as beat
+from cronus.timeout import timeout,TimeoutError 
 
 from apiclient import discovery
 from oauth2client import client as oauth2client
 
 DEBUG = True
-
 PUBSUB_SCOPES = ['https://www.googleapis.com/auth/pubsub']
+arduinoIP = os.environ['arduino1']
+ = os.environ['arduino1']
 
+def createCredentials():
+
+
+@timeout(20)
 def create_pubsub_client(http=None):
     try:
         credentials = oauth2client.GoogleCredentials.get_application_default()
     except (ApplicationDefaultCredentialsError, ValueError) as error:
-        print "Load credentials failed with error: " + error
+        if DEBUG: print "Load credentials failed with error: " + error
     if credentials.create_scoped_required():
         credentials = credentials.create_scoped(PUBSUB_SCOPES)
     if not http:
@@ -25,35 +32,67 @@ def create_pubsub_client(http=None):
 
     return discovery.build('pubsub', 'v1', http=http)
 
-arduinoIP = os.environ['arduino1']
+@timeout(10)
+def getSensorValue(self,IP):
+    try:
+        if DEBUG: print "Sending http request to arduino"
+        response = requests.get("http://" + IP + "/")
+    except requests.exceptions.RequestException as e:
+        if DEBUG : print e
+        return Null
+    else:
+        if DEGUG: print response.json()
+        return response
+    ##    sys.exit()
 
-try:
-    response = requests.get("http://" + arduinoIP + "/")
-except requests.exceptions.RequestException as e:
-    if DEBUG :
-        print e
-##    sys.exit()
+@timeout(3)
+def formatMessage(self,arduinoResponse):
+    resp = arduinoResponse.json()
+    humidity = resp['variables']['humidity']
+    if DEBUG: print "Collected humidity value: " + str(humidity)
+    message1 = base64.b64encode(str(humidity))
+    body = {
+        'messages': [
+            {'data': message1},
+        ]
+    }
+    return body
+
+@timeout(10)
+def writeMessageToMQ(self,message,client):
+    # Send the humidity value to message queue
+    resp = client.projects().topics().publish(
+        topic='projects/gardencontrolarduino/topics/humidity', body=message).execute()
 
 
-resp = response.json()
+if __name__ == "__main__":
+    try:
+        client = create_pubsub_client()
+    except TimeoutError:
+        if DEBUG: print "Timeout creating pubsub client"
+    else:
+        beat.set_rate(0.0333333333)
+        while beat.true:
+            try:
+                response = getSensorValue(arduinoIP)
+            except TimeoutError:
+                if DEBUG: print "Timeout reading sensor value API"
+            else:
+                try:
+                    body = formatMessage(response)
+                except TimeoutError:
+                    if DEBUG: print "Timeout formating message"
+                else:
+                    try:
+                        writeMessageToMQ(body,client)
+                    except TimeoutError:
+                        if DEBUG: print "Timeout sending message to MQ"
+        beat.sleep()
 
-humidity = resp['variables']['humidity']
-if DEBUG:
-    print "Collected humidity value: " + str(humidity)
 
-# Send the humidity value to message queue
-client = create_pubsub_client()
 
-message1 = base64.b64encode(str(humidity))
 
-body = {
-    'messages': [
-        {'data': message1},
-    ]
-}
 
-resp = client.projects().topics().publish(
-    topic='projects/gardencontrolarduino/topics/humidity', body=body).execute()
 
 
 
